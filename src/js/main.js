c2e58b3b08b77c1ac5a94ee24da842c8514334df
Dragneel1337 <paw.drozd@interia.pl>
2023-07-31 16:54:47 +0200

    navbar and header mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,29 @@
 const burgerBtn = document.querySelector('.burger-btn');
-const nav = document.querySelector('.nav')
-const allNavItems = document.querySelectorAll('.nav__item')
+const nav = document.querySelector('.nav__box')
+const allNavItems = document.querySelectorAll('.nav__box--item')
 
 
 const handleNav = () => {
     burgerBtn.classList.toggle('active');
-    nav.classList.toggle('nav--active')
+    nav.classList.toggle('show-nav')
 
     allNavItems.forEach(item => {
         item.addEventListener('click', () => {
-            nav.classList.remove('nav--active')
+            nav.classList.remove('show-nav')
             burgerBtn.classList.remove('active')
         })
     });
+    handleNavItemsAnimation()
+}
+
+const handleNavItemsAnimation = () => {
+    let delayTime = 0;
+
+    allNavItems.forEach(item => {
+        item.classList.toggle('nav-animation')
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
+    })
 }
 
 
